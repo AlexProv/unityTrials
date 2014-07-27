71f9c7bbf4512e51d7a9a833906ccae3a16d72d7
AlexProv <alex.provencher@usherbrooke.ca>
2014-07-27 00:46:24 -0400

    AutoPush

--- Solution2/blockController.cs
+++ Solution2/blockController.cs
@@ -20,34 +20,26 @@ public class blockController : MonoBehaviour {
 		//float lockPos = 0;
 		//transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos);
 		//transform.LookAt(new Vector3(transform.position.x,-1,transform.position.z));
-		
-		/*
-		
-		*/
 	}
 	
 	void OnTriggerEnter(Collider other)
 	{
 		//print (other.gameObject.name);
-		//demo = GameObject.Instantiate(type) as GameObject;
-		//demo.transform.parent = other.transform;
+		demo = GameObject.Instantiate(type) as GameObject;
+		demo.transform.parent = other.transform;
 		
-		//A pyramid 
-		changeParentChild(a,b);			
-		GameObject c = b;
-		b = a;
-		a = c;
-		//B pyramid 
+		Transform tn = demo.transform.GetChild(0) as Transform;
+		changeParentChild(demo,tn.gameObject);
 		
-		a.transform.parent = other.transform.parent;
-		a.transform.localPosition = Vector3.zero;
-		a.transform.localRotation = Quaternion.identity;
+		tn.transform.parent = other.transform.parent;
+		tn.transform.localPosition = Vector3.zero;
+		tn.transform.localRotation = Quaternion.identity;
 		
 	}
 	
 	void OnTriggerExit()
 	{
-		//Destroy(demo);
+		Destroy(demo);
 	}
 	
 	void changeParentChild(GameObject parent, GameObject child)
