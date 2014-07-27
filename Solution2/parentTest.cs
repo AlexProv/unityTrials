2e843a949e8b65374a1c0b3d52d1d59201f43158
AlexProv <alex.provencher@usherbrooke.ca>
2014-07-27 00:27:14 -0400

    AutoPush

---
+++ Solution2/parentTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parentTest : MonoBehaviour {
+	public GameObject child;
+	
+	Queue q;
+	// Use this for initialization
+	void Start () {
+		q = new Queue();
+		foreach(Transform t in gameObject.transform)
+		{
+			q.Enqueue(t);
+			print(t.name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			Vector3 pos = child.transform.localPosition;
+			Quaternion rot = child.transform.localRotation;
+			Transform t = q.Dequeue() as Transform;
+			child.transform.parent = t;
+			child.transform.localPosition = pos;
+			child.transform.localRotation = rot;
+			q.Enqueue(t);
+		}	
+	}
+}
