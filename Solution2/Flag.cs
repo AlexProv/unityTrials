79cfbda0c89346f275de2dcab44127f52a0b0b48
AlexProv <alex.provencher@usherbrooke.ca>
2014-08-16 18:19:54 -0400

    AutoPush

---
+++ Solution2/Flag.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flag : MonoBehaviour {
+
+	void Awake()
+	{}
+	
+	string value{private set; public get;}
+	
+	public void setFlag()
+	{
+	
+	}
+	
+}
